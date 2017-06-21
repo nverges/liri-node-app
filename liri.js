37cf6e6e336ccefdea5547fa1e6c429756efd09d
nverges <nverges@email.arizona.edu>
2017-06-21 16:57:35 -0700

    update

--- liri.js
+++ liri.js
@@ -10,17 +10,17 @@ const fs = require('fs');
 const omdb = require('omdbapi');
 const twitter = require('twitter');
 const spotify = require('node-spotify-api');
+const request = require('request');
 
 // requires the keys.js file
-var keys = require('./keys');
+var keys = require('./keys.js');
 
 // // OMDB
 // var omdbKeys = keys.omdbKeys;
 // var omdbAccess = new omdb(omdbKeys);
 
-// API key values pulled from keys.js
-var twitterKeys = keys.twitterKeys;
-var twitterAccess = new twitter(twitterKeys);
+// Twitter 
+var twitterAuth = keys.twitterKeys;
 
 // Spotify
 var spotifyKeys = keys.spotifyKeys;
@@ -28,8 +28,7 @@ var spotifyAccess = new spotify(spotifyKeys);
 
 // user inputs command to perform a desired action
 var command = process.argv[2];
-var query = process.argv[3];
-
+var inputQuery = process.argv[3];
 
 
 ///////////////////////////////////////
@@ -70,61 +69,59 @@ switch (command) {
 		console.log('default');
 };
 
-
-
 ///////////////////////////////////////
 ///////////// FUNCTIONS ///////////////
 ///////////////////////////////////////
 
 // performs twitter search
-function twitterAction() {
+function twitterAction(command) {
+
+	// creates new twitter object, passing in auth keys from keys.js
+	var auth = new twitter(twitterAuth);
+
+	// creates search query
 	var params = {screen_name: 'nvTweetsJS'};
-	twitterAccess.get('statuses/user_timeline', params, function(error, tweets, response) {
+	auth.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
-	  	console.log(params);
 	    console.log(tweets);
 	    console.log(JSON.stringify(response, null, 2));
+	  } else {
+	  	console.log('There was an error');
 	  }
 	});
 };
 
 // performs spotify search
-function spotifyAction() {
-	spotifyAccess
-	  .search({ type: 'track', query: 'All the Small Things' })
-	  .then(function(response) {
-	    console.log(response);
-	  })
-	  .catch(function(err) {
-	    console.log(error);
-	  });
+function spotifyAction(command, inputQuery) {
+	 
+	var spotifyQuery = new spotify({
+	  id: '416e15d0a9c74b8088ff7c34a0029097',
+	  secret: '16fa8954ec104e69b80bdde209936695'
+	});
+	 
+	spotifyQuery.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	 
+	console.log(JSON.stringify(data, null, 2)); 
+
+	// fs.writeFile('json.txt', )
+	});
 };
 
 // performs omdb search
 function movieAction() {
-		omdb.search({
-	    search: 'game of thrones',  // required 
-	    type: 'series',             // optionnal  ['series', 'episode', 'movie'] 
-	    year: '2011',               // optionnal 
-	    page: '1'                   // optionnal (1 to 100) 
-	}).then(res => {
-	    console.log('got response:', JSON.stringify(res, null, 2));
-	}).catch(console.error);
-	 
-	omdb.get({
-	    id: '40e9cece',            // optionnal (requires imdbid or title) 
-	    title: 'Game of Thrones',   // optionnal (requires imdbid or title) 
-	    season: 1,                  // optionnal 
-	    episode: 1,                 // optionnal 
-	    type: 'series',             // optionnal ['series', 'episode', 'movie'] 
-	    plot: 'full',               // optionnal (defaults to 'short') 
-	    tomatoes: true,             // optionnal (get rotten tomatoes ratings) 
-	    year: '2011'                // optionnal 
-	}).then(res => {
-	    console.log('got response:', JSON.stringify(res, null, 2));
-	}).catch(console.error);
 
+	var movieSearch = request(("http://www.omdbapi.com/?t=" + inputQuery + "&y=&plot=short&r=json&apikey=40e9cece"), function (err, response, body) {
+		if (err) throw err;
+		console.log(JSON.stringify(response, null, 2));
+		var body = response.body;
+		console.log(`MOVIE TITLE: ${body.title}`);
+	});
 };
+
+
 
 // performs specified action
 function doAction() {
