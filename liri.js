04337b3378348ea905afc562bd2a3371dc67efbb
nverges <nverges@email.arizona.edu>
2017-06-23 21:23:19 -0700

    note

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
+/////// TO DO //////////////
+// set default movie to 'Mr. Nobody' in movie-this
+// set default song to 'Ace of Base' in spotify-this-song
+
+
+
 ///////////////////////////////////////
 ///////////// VARIABLES ///////////////
 ///////////////////////////////////////
@@ -10,20 +16,18 @@ var keys = require('./keys.js');
 
 // packages
 const fs = require('fs');
-const omdb = require('omdbapi');
 var Twitter = require('twitter');
 
 const spotify = require('node-spotify-api');
 const request = require('request');
-
 
 // Spotify
 var spotifyKeys = keys.spotifyKeys;
 var spotifyAccess = new spotify(spotifyKeys);
 
 // user inputs command to perform a desired action
-var command = process.argv[2];
-var inputQuery = process.argv[3];
+let command = process.argv[2];
+let inputQuery = process.argv[3];
 
 
 ///////////////////////////////////////
@@ -33,6 +37,7 @@ var inputQuery = process.argv[3];
 
 switch (command) {
 	case 'my-tweets':
+		console.log(' ');
 		console.log('TWEETS');
 		console.log('__________________________________');
 		console.log('__________________________________');
@@ -40,6 +45,7 @@ switch (command) {
 	break;
 
 	case 'spotify-this-song':
+		console.log(' ');
 		console.log('SPOTIFY');
 		console.log('__________________________________');
 		console.log('__________________________________');
@@ -47,6 +53,7 @@ switch (command) {
 	break;
 
 	case 'movie-this':
+		console.log(' ');
 		console.log('MOVIE');
 		console.log('__________________________________');
 		console.log('__________________________________');
@@ -54,9 +61,10 @@ switch (command) {
 	break;
 
 	case 'do-what-it-says':
-		// console.log('DO WHAT IT SAYS');
-		// console.log('__________________________________');
-		// console.log('__________________________________');
+		console.log(' ');
+		console.log('DO WHAT IT SAYS');
+		console.log('__________________________________');
+		console.log('__________________________________');
 		doAction();
 	break;
 
@@ -74,14 +82,16 @@ switch (command) {
 function twitterAction(command) {
 
 	// creates new twitter object, passing in auth keys from keys.js
-	var client = new Twitter(keys.twitterKeys);
+	let client = new Twitter(keys.twitterKeys);
 
 	// creates search query
 	var params = {screen_name: 'nvTweetsJS'};
+
+	// creates request and prints data
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
 			// console.log(tweets);
-			for (var i = 0; i < tweets.length; i++) {
+			for (let i = 0; i < tweets.length; i++) {
 				console.log(tweets[i].created_at);
 				console.log(' ');
 				console.log(tweets[i].text);
@@ -100,7 +110,7 @@ var getArtistNames = function(artist) {
 // performs spotify search
 function spotifyAction(command) {
 	 
-	var spotifyQuery = new spotify({
+	let spotifyQuery = new spotify({
 	  id: '416e15d0a9c74b8088ff7c34a0029097',
 	  secret: '16fa8954ec104e69b80bdde209936695'
 	});
@@ -133,8 +143,8 @@ function spotifyAction(command) {
 function movieAction() {
 
 	// call to omdb server using "request" package
-	var apiKey = '40e9cece';
-	var movieSearch = request(("http://www.omdbapi.com/?t=" + inputQuery + "&y=&plot=short&r=json&apikey=" + apiKey), function (err, response, body) {
+	const apiKey = '40e9cece';
+	let movieSearch = request(("http://www.omdbapi.com/?t=" + inputQuery + "&y=&plot=short&r=json&apikey=" + apiKey), function (err, response, body) {
 		
 		// check for errors
 		if (err) throw err;
@@ -142,20 +152,26 @@ function movieAction() {
 		// console.log(JSON.stringify(response, null, 2));
 
 		// grabs json response and stores in a variable
-		var jsonData = JSON.parse(body);
+		let jsonData = JSON.parse(body);
 
-		// logs out movie info
-		console.log(' ');
-		console.log(`Movie Title: ${jsonData.Title}`);
-		console.log(`Year: ${jsonData.Year}`);
-		console.log(`Rating: ${jsonData.Rated}`);
-		console.log(`Runtime: ${jsonData.Runtime}`)
-		console.log(`Director: ${jsonData.Director}`)
-		console.log(`Actors: ${jsonData.Actors}`)
-		console.log(`Plot: ${jsonData.Plot}`);
-		console.log(`IMDB Rating: ${jsonData.imdbRating}`);
-		console.log(' ');
-	 	console.log('__________________________________');
+		if (inputQuery === '') {
+			inputQuery === "Mr. Nobody";
+			console.log('inputQuery: ' + inputQuery)
+
+		} else {
+
+			// logs out movie info
+			console.log(' ');
+			console.log(`Movie Title: ${jsonData.Title}`);
+			console.log(`Year: ${jsonData.Year}`);
+			console.log(`IMDB Rating: ${jsonData.imdbRating}`);
+			console.log(`Rating: ${jsonData.Rated}`);
+			console.log(`Country: ${jsonData.Country}`);
+			console.log(`Plot: ${jsonData.Plot}`);
+			console.log(`Actors: ${jsonData.Actors}`);
+			console.log(' ');
+		 	console.log('__________________________________');
+	 	}
 
 	});
 };
@@ -164,13 +180,47 @@ function movieAction() {
 // performs do-what-it-says action
 // pulls data from random.txt
 function doAction() {
-	console.log('DO WHAT IT SAYS');
-	console.log('__________________________________');
-	console.log('__________________________________');
 
-	fs.readFile('random.txt', 'utf8', function(error, data) {
-		if (error) throw error;
-		console.log(data);
+	// .txt file
+	const randomTxt = 'random.txt';
+
+	fs.readFile(randomTxt, 'utf8', function(error, data) {
+
+		// displays input
+		console.log(`Here's what it says to do: ${data}`)
+
+		// error handling
+		if (error) {
+			console.log('There was an error. Unable to find: ' + randomTxt);
+		} else {
+
+			// splits content into an array at each comma
+			let txtContent = data.split(',');
+
+			command = txtContent[0];
+			inputQuery = txtContent[1];
+			
+			switch (command) {
+				case 'my-tweets':
+					twitterAction();
+				break;
+
+				case 'spotify-this-song':
+					spotifyAction();
+				break;
+
+				case 'movie-this':
+					movieAction();
+				break;
+
+				case 'do-what-it-says':
+					doAction();
+				break;
+
+				case 'default':
+					console.log('default');
+			};
+		};
 	});
 };
 
